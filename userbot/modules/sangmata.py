f0ace31829ca49aa871180c898fc10230090e3fa
F I Z I • Ɱeƙípres <91390078+zeinzo@users.noreply.github.com>
2021-12-08 22:12:07 +0700

    kurang })

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -1,9 +1,4 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-# Port to userbot by @MoveAngel
+
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from userbot import bot, CMD_HELP
